e23829fdf70fc7f4a6c586e73badd8e7da338287
Demon <d3m0nkingx@gmail.com>
2019-03-19 16:59:21 +0000

    SEED

--- php/config.sample.php
+++ php/config.sample.php
@@ -1,85 +1,88 @@
 <?php
 /**
- * Bitcoin Status Page
+ * SEED Status Page
  *
  * @category File
- * @package  BitcoinStatus
+ * @package  BitcoinStatus | SEEDStatus
  * @author   Craig Watson <craig@cwatson.org>
+ * @author   SEED Project <info@seednetwork.io>
  * @license  https://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
  * @link     https://github.com/craigwatson/bitcoind-status
+ * @link     https://github.com/SEEDPlatform/SEEDd-status
  */
 
 $config = array(
+
   // RPC
   'rpc_user'                  => 'rpcuser',
   'rpc_pass'                  => 'rpcpass',
-  'rpc_host'                  => 'localhost',
-  'rpc_port'                  => '8332',
+  'rpc_host'                  => '0.0.0.0',
+  'rpc_port'                  => '42421',
   'rpc_ssl'                   => false,
   'rpc_ssl_ca'                => null,
 
   // Donations
-  'display_donation_text'     => false,
-  'donation_address'          => 'not_set',
-  'donation_amount'           => '0.001',
+  'display_donation_text'     => true,
+  'donation_address'          => '<SEED Address>',
+  'donation_amount'           => '1.0',
 
   // Peers
-  'display_peer_info'         => false,
-  'display_peer_port'         => false,
+  'display_peer_info'         => true,
+  'display_peer_port'         => true,
   'hide_dark_peers'           => true,
   'ignore_unknown_ping'       => false,
   'peers_to_ignore'           => array(),
 
   // Cache
-  'cache_geo_data'            => false,
-  'geo_cache_file'            => '/var/tmp/bitcoind-geolocation.cache',
+  'cache_geo_data'            => true,
+  'geo_cache_file'            => '/var/tmp/SEEDd-geolocation.cache',
   'geo_cache_time'            => 604800,
   'use_cache'                 => true,
-  'cache_file'                => '/tmp/bitcoind-status.cache',
+  'cache_file'                => '/tmp/SEEDd-status.cache',
   'max_cache_time'            => 300,
   'nocache_whitelist'         => array('127.0.0.1'),
 
   // Geolocation
-  'geolocate_peer_ip'         => false,
-  'display_ip_location'       => false,
+  'geolocate_peer_ip'         => true,
+  'display_ip_location'       => true,
 
   // UI
   'display_ip'                => false,
-  'display_free_disk_space'   => false,
-  'display_testnet'           => false,
+  'display_free_disk_space'   => true,
+  'display_testnet'           => true,
   'display_version'           => true,
   'display_github_ribbon'     => true,
-  'display_max_height'        => true,
+  'display_max_height'        => false,
   'use_bitcoind_ip'           => true,
-  'intro_text'                => 'not_set',
-  'title_text'                => 'Bitcoin Node Status',
+  'intro_text'                => 'This is the SEED Node Information Site',
+  'title_text'                => 'SEED Node Status',
   'display_bitnodes_info'     => false,
-  'display_chart'             => false,
-  'display_peer_chart'        => false,
+  'display_chart'             => true,
+  'display_peer_chart'        => true,
   'node_links'                => array(),
 
   // Stats
   'stats_whitelist'           => array('127.0.0.1'),
-  'stats_file'                => '/tmp/bitcoind-status.data',
+  'stats_file'                => '/tmp/SEEDd-status.data',
   'stats_max_age'             => '604800',
   'stats_min_data_points'     => 5,
 
   // Node Count
   'peercount_whitelist'       => array('127.0.0.1'),
-  'peercount_file'            => '/tmp/bitcoind-peers.data',
+  'peercount_file'            => '/tmp/SEEDd-peers.data',
   'peercount_max_age'         => '2592000',
   'peercount_min_data_points' => 10,
   'peercount_extra_nodes'     => array(),
 
   // Uptime
-  'display_bitcoind_uptime'   => false,
-  'bitcoind_process_name'     => 'bitcoind',
+  'display_bitcoind_uptime'   => true,
+  'bitcoind_process_name'     => 'SEEDd',
 
   // System
   'date_format'               => 'H:i:s T, j F Y ',
   'disk_space_mount_point'    => '.',
   'timezone'                  => null,
-  'stylesheet'                => 'v2-light.css',
+  'stylesheet'                => 'v2-dark.css',
   'debug'                     => false,
-  'admin_email'               => 'admin@example.com',
+  'admin_email'               => 'info@seednetwork.io',
 );
